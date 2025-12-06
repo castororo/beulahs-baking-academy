574492ffcf05b5e3074520d427e406eb0c08fd0c
JasonJoshua-vei <jasonjoshuaveitechnologies@gmail.com>
2025-12-06 23:29:19 +0530

    refresh fix for academy page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import ErrorBoundary from "@/components/ErrorBoundary";
@@ -23,7 +23,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <HashRouter>
+        <BrowserRouter>
           <div className="flex flex-col min-h-screen">
             <ErrorBoundary>
               <Header />
@@ -85,7 +85,7 @@ const App = () => (
               <Footer />
             </ErrorBoundary>
           </div>
-        </HashRouter>
+        </BrowserRouter>
       </TooltipProvider>
     </QueryClientProvider>
   </ErrorBoundary>
